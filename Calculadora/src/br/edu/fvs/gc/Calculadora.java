cb84e9ce597e3b2e3ae1fea7e278eeb9b14daa88
victorlima395 <victorlima395@gmail.com>
2020-04-26 13:32:09 -0300

    convercao de celsius para farenheit

--- Calculadora/src/br/edu/fvs/gc/Calculadora.java
+++ Calculadora/src/br/edu/fvs/gc/Calculadora.java
@@ -1,6 +1,13 @@
 package br.edu.fvs.gc;
 
-
 public class Calculadora {
-    
+
+    public double converterFarenheitParaCelcius() {
+
+        double celcius = 38;
+
+        double resultado = (celcius * 1.8) + 32;
+        return resultado;
+    }
+
 }
