bfe8c6d874f8328866f200000bc83da66ce224f7
Alan Pery <silvaalan121@gmail.com>
2020-04-26 10:22:49 -0300

    Adicionando a calculadora ao projeto

---
+++ AtividadeGitMerging/src/br/edu/fvs/gc/CalcularIMC.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fvs.gc;
+
+/**
+ *
+ * @author pessoal
+ */
+public class CalcularIMC {
+    public int calcularIMC(double altura, double peso){
+        altura = 10;
+        peso = 20;
+        return (int) ((altura * 2)/peso);
+    }
+}
